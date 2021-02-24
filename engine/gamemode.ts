adecf0bfa0d641800d193fc2cfd20105802b90ae
Sascha Sallès <sascha.salles@ynov.com>
2021-02-24 21:49:52 +0100

    fetchPlayers method

--- engine/gamemode.ts
+++ engine/gamemode.ts
@@ -1,3 +1,30 @@
-abstract class GameMode {
+import axios from "axios";
+
+
+
+export class GameMode {
+  
+  public players: Array<Players>
+
+
+  constructor() {
+    this.players = this.fetchPlayers()
+  }
+
+
+  fetchPlayers(): Players[] {
+    const players: Players[]
     
+    const req = axios.get('http://localhost:3000/players')
+    req.then((response) => {
+      response.data.map( player => {
+        players.push(new Player(player.id, player.name, player.gameWin, player.gameLost, player.createdAt))
+      })
+
+      console.log(response.status);
+    });
+
+    return players
+  }
 }
+
--- engine/gamemodes/301.ts
+++ engine/gamemodes/301.ts
@@ -1,3 +1,4 @@
 class Mode1 extends GameMode {
 
+  
 }
