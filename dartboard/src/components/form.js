6b228fa6a89fe4c9096c832de73e2d269142a9b3
Sascha Sallès <sascha.salles@ynov.com>
2021-02-28 12:57:55 +0100

    :sparkles: improve UI and ad post game feature

--- dartboard/src/components/form.js
+++ dartboard/src/components/form.js
@@ -1,18 +1,106 @@
-import React from 'react';
-import { Button, Form } from 'semantic-ui-react';
+import { useState, useEffect, React } from "react";
+import { Button, Form, List, Segment, Message } from "semantic-ui-react";
 
-const PlayerForm = () => (
-  <Form>
-    <Form.Field>
-      <label>First Name</label>
-      <input placeholder='Name' />
-    </Form.Field>
-    <Form.Field>
-      <label>Email</label>
-      <input type="email" placeholder='Email' />
-    </Form.Field>
-    <Button type='submit'>Add Player</Button>
-  </Form>
-)
 
-export default PlayerForm;
+
+const initialValues = {
+  name: "",
+  email: "",
+};
+
+export function PlayerForm(props) {
+  const [values, setValues] = useState(initialValues);
+  const [players, setPlayers] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleAddPlayer = (player) => {
+    const newPlayers = [...players];
+    newPlayers.push(player);
+    setPlayers(newPlayers);
+    resetInitialValue();
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    handleAddPlayer(values.name);
+  };
+
+  const resetInitialValue = () => {
+    setValues({ name: "", email: "" });
+  };
+
+  const handleDelete = (evt, index) => {
+    const newPlayers = [...players];
+
+    newPlayers.splice(index, 1);
+
+    setPlayers(newPlayers);
+  };
+
+  return (
+    <div>
+      <Segment>
+        <List selection>
+          {players.length == 0 && (
+            <Message>
+              <p>Veuillez ajouter au moins 2 joueurs </p>
+            </Message>
+          )}
+          {players.map((name, index) => (
+            <List.Item verticalAlign="bottom">
+              <List.Content key={index} floated="right">
+                <Button
+                  key={index}
+                  color="red"
+                  circular
+                  icon="trash alternate"
+                  verticalAlign="middle"
+                  onClick={(e, index) => handleDelete()}
+                ></Button>
+              </List.Content>
+              <List.Content>
+                Joueur {index + 1} : {name}
+              </List.Content>
+            </List.Item>
+          ))}
+        </List>
+      </Segment>
+
+      <Form onSubmit={handleSubmit}>
+        <Form.Field>
+          <label>Nom</label>
+          <input
+            required
+            placeholder="Nom"
+            value={values.name}
+            name="name"
+            onChange={handleInputChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Email</label>
+          <input
+            required
+            type="email"
+            placeholder="Email"
+            value={values.email}
+            name="email"
+            onChange={handleInputChange}
+          />
+        </Form.Field>
+        <Button type="submit">Ajouter un joueur</Button>
+        <Button color="purple" icon="game" content="Jouer" />
+      </Form>
+
+
+    </div>
+  );
+}
